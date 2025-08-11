07206bf947bfa79a476216ba8bab8324ec714eb6
Jair Pérez <oquendojair10@gmail.com>
2025-08-11 16:59:17 -0500

    feat refactor footer

--- app/page.tsx
+++ app/page.tsx
@@ -302,17 +302,18 @@ export default function Home() {
             Hecho con
           </span>
           <svg
+            xmlns="http://www.w3.org/2000/svg"
             width="20"
             height="20"
-            viewBox="0 0 20 20"
+            viewBox="0 0 24 24"
             fill="none"
-            className="text-black/50"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide lucide-heart-icon lucide-heart text-red-500/50"
           >
-            <path
-              d="M17.5 6.25C17.5 4.18 15.82 2.5 13.75 2.5C12.36 2.5 11.14 3.31 10.5 4.5C9.86 3.31 8.64 2.5 7.25 2.5C5.18 2.5 3.5 4.18 3.5 6.25C3.5 12.5 10.5 17.5 10.5 17.5S17.5 12.5 17.5 6.25Z"
-              stroke="currentColor"
-              strokeWidth="2"
-            />
+            <path d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5" />
           </svg>
           <span className="text-base font-semibold text-black/50">
             por Jair Pérez

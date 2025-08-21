18f550cf643ae63599224fba996d00002cc66615
Jair Pérez <oquendojair10@gmail.com>
2025-08-21 09:19:33 -0500

    fix warnings

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,7 @@ export default function Home() {
               </h1>
 
               <div className="bg-cv-orange/30 inline-block px-2 py-1 rounded">
-                <h2 className="text-xl lg:text-2xl font-bold text-black/35 leading-normal tracking-tight leading-tight">
+                <h2 className="text-xl lg:text-2xl font-bold text-black/35  tracking-tight leading-tight">
                   Centrando las voces de los residentes a través
                   <br className="hidden md:block" />
                   de la investigación participativa
